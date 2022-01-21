9b26fc07166f5e9f752898193d34e40108f152b5
ds1242 <david.shaw1242@gmail.com>
2022-01-20 21:13:33 -0700

    solved section 6 algorithms

--- 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
+++ 06-Server-Side-APIs/03-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,10 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+    let total = 1;
+    for(let i = num; i > 0; i--) {
+        total = total * (i);
+    }
+    console.log(total);
+    return total;
+};
