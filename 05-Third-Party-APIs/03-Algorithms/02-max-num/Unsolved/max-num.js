d75beabe7ddf99aa6385a20b23fb98b3ef8e9ba1
ds1242 <david.shaw1242@gmail.com>
2022-01-20 20:46:18 -0700

    completed section 5 algorithms

--- 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
+++ 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,11 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    let highest = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] > highest) {
+            highest = arr[i];
+        }        
+    }
+    return highest;
+};
--- 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 05-Third-Party-APIs/03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,13 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    let count = 0;
+    let strArr = str.split('');
+    for (let i = 0; i < strArr.length; i++) {
+        if(/^[aeiou]/.test(strArr[i].toLowerCase()) === true) {
+            count += 1;
+        }        
+    }
+    console.log(count);
+    return count;
+};
