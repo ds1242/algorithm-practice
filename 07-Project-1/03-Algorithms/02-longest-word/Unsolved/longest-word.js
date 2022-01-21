e26ed952cd63b0cabdd5190f98ac0573c115a6a2
ds1242 <david.shaw1242@gmail.com>
2022-01-21 16:25:27 -0700

    finished longest word from section 7

--- 07-Project-1/03-Algorithms/02-longest-word/Unsolved/longest-word.js
+++ 07-Project-1/03-Algorithms/02-longest-word/Unsolved/longest-word.js
@@ -1,3 +1,27 @@
 // Write code to create a function that accepts a string (`str`) and returns the longest word in the string
 
-var longestWord = function(str) {};
+var longestWord = function(str) {
+    let wordsArr = str.split(' ');
+    
+    let longest = 0;
+    let currentCount = 0;
+    let longestWord;
+    
+    for (let i = 0; i < wordsArr.length; i++) {
+        let word = wordsArr[i].split('');
+        // console.log(word)
+        
+        for (let j = 0; j < word.length; j++) {
+            currentCount = currentCount + 1;
+            // console.log(currentCount)
+        } 
+        if(currentCount > longest) {
+            longest = currentCount;
+            longestWord = wordsArr[i];
+            // console.log(longest)
+        }
+        currentCount = 0;    
+        // console.log(longestWord)       
+    }
+    return longestWord;
+};
