5c328e635e6139b4c997844eee38413ef8c72445
ds1242 <david.shaw1242@gmail.com>
2022-01-20 21:18:12 -0700

    completed swap case in section 7

--- 07-Project-1/03-Algorithms/01-swap-case/Unsolved/swap-case.js
+++ 07-Project-1/03-Algorithms/01-swap-case/Unsolved/swap-case.js
@@ -1,3 +1,13 @@
 // Write code to create a function takes a string and returns the string with all of the letter cases swapped
 
-var swapCase = function(str) {};
+var swapCase = function(str) {
+    let strArr = str.split('')
+    for (let i = 0; i < strArr.length; i++) {
+        if(strArr[i] === strArr[i].toLowerCase()) {
+            strArr[i] = strArr[i].toUpperCase();
+        } else if ( strArr[i] === strArr[i].toUpperCase()) {
+            strArr[i] = strArr[i].toLowerCase();
+        }        
+    }
+    return strArr.join('')
+};
