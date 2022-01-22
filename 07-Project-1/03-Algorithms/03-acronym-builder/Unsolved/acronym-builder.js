25b67b9ea40a5f7b5a0b39bd3969d6797c4f7faa
ds1242 <david.shaw1242@gmail.com>
2022-01-22 13:19:59 -0700

    finished section 7

--- 07-Project-1/03-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
+++ 07-Project-1/03-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
@@ -1,3 +1,18 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
-var acronymBuilder = function(str) {};
+var acronymBuilder = function(str) {
+    let words = str.split(' ');
+    let acronym = [];
+
+    for (let i = 0; i < words.length; i++) {
+
+        let word = words[i].split('');
+
+        if(word[0]) {
+            acronym.push(word[0]);
+        }
+        
+    }
+
+    return acronym.join('').toUpperCase();
+};
--- 07-Project-1/03-Algorithms/04-reverse-words/Unsolved/reverse-words.js
+++ 07-Project-1/03-Algorithms/04-reverse-words/Unsolved/reverse-words.js
@@ -1,3 +1,7 @@
 // Write code to create a function that accepts a string (`str`) and reverses the order of the words in the string
 
-var reverseWords = function(str) {};
+var reverseWords = function(str) {
+    //stack the js methods to do all in one line 
+    return str.split(' ').reverse().join(' ');
+
+};
