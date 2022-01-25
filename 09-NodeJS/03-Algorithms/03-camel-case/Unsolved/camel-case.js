6ed4c7d542660e81996d7e00b2cfca7e4b466ede
ds1242 <david.shaw1242@gmail.com>
2022-01-25 08:24:46 -0700

    started camelcase alorgithm in section 9

--- 09-NodeJS/03-Algorithms/03-camel-case/Unsolved/camel-case.js
+++ 09-NodeJS/03-Algorithms/03-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,22 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function(str) {
+    let strArr = str.split(' ');
+    let result = []
+    result.push(strArr[0].charAt(0).toLowerCase());
+    for (let i = 1; i < strArr.length; i++) {
+        let word = strArr[i];
+        for (let j = 0; j < word.length; j++) {
+            if(j === 0) {
+                word[0] = word[0].toUpperCase();
+            } else {
+                word[j] = word[j]
+            }
+            
+        }
+        
+        result.push(word);        
+    }
+    console.log(result.join(''));
+    return result.join('')
+};
