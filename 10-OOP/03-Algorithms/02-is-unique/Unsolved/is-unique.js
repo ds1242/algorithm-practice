927ce2397da8dfdbd90fab53476aecbe833cfe41
ds1242 <david.shaw1242@gmail.com>
2022-01-27 10:27:10 -0700

    finished is-unique started work on is-anagram

--- 10-OOP/03-Algorithms/02-is-unique/Unsolved/is-unique.js
+++ 10-OOP/03-Algorithms/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,18 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+    let output = true;
+    for (let i = 0; i < arr.length; i++) {
+        let currentNum = arr[i];
+        for (let j = 0; j < arr.length; j++) {
+            if(currentNum === arr[j] && j !== i) {
+                output = false;
+            }
+            
+        }
+        
+    }
+    return output;
+
+};
--- 10-OOP/03-Algorithms/03-is-anagram/Unsolved/is-anagram.js
+++ 10-OOP/03-Algorithms/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,8 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+var isAnagram = function(strA, strB) {
+    if(strA.length !== strB.length) {
+        return false;
+    }
+};
