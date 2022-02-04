ffac7dd32ead7b0669106ee5af191b2429f3222b
ds1242 <david.shaw1242@gmail.com>
2022-02-04 13:48:55 -0700

    finished 2d array search algorithm

--- 11-Express/03-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
+++ 11-Express/03-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
@@ -2,4 +2,19 @@
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
 
-var arraySearch2D = function(arr) {};
+var arraySearch2D = function(arr) {
+
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let currentRow = arr[i];
+        for (let j = 0; j < currentRow.length; j++) {
+            let currentElement = currentRow[j];
+            if(currentElement === 'X') {
+                count++;
+            }            
+        }        
+    };
+    return count;
+
+
+};
