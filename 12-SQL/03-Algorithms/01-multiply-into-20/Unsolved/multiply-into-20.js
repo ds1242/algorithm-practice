863a45d89cbc861f4112b13af2085db0e883f184
ds1242 <david.shaw1242@gmail.com>
2022-02-17 15:23:00 -0700

    started psuedocoding  the solution

--- 12-SQL/03-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
+++ 12-SQL/03-Algorithms/01-multiply-into-20/Unsolved/multiply-into-20.js
@@ -1,4 +1,42 @@
 // Write code to create a function that accepts an array of unique numbers
 // If any two numbers in the array add up to 20, return true, else return false
+// loop through the array selecting an element
+// take an element of the array
+// multiply by every other element in 
 
-var multiplyInto20 = function(arr) {};
+var multiplyInto20 = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const element = array[i];
+        
+    }
+
+};
+
+
+
+
+// function minimizeBias(ratings) {
+//     // Write your code here
+    
+//     // if array is only 1 element return 0
+//     if(ratings.length === 1) {
+//         return 0;
+//     }
+//     // sort the array
+//     ratings.sort();
+    
+//     // set difference of the first two elements in the array
+//     let difference = ratings[1] - ratings[0];
+//     let bias = 0;
+//     console.log(ratings);
+//     for(let i = 2; i < ratings.length; i++) {
+//         difference = Math.min(difference, ratings[i] - ratings[i-1])
+//         if(difference > 0 && difference !== (ratings[i] - ratings[i - 1])){
+//             bias = bias++;
+//         } else if (difference === (ratings[i] - ratings[i - 1])){
+//             bias = bias + difference;
+//         }
+//         console.log(bias);
+//     }
+//     return bias;
+// }
