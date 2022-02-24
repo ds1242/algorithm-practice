ad3466156260eab23234ee5aba6a0284b55d0eaa
ds1242 <david.shaw1242@gmail.com>
2022-02-23 20:37:32 -0700

    initial setup of zeroes and ones count alrogithm

--- 12-SQL/03-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ 12-SQL/03-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,20 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+var zeroesAndOnes = function(str) {
+    let strArr = str.split('');
+    let zeroCount = 0;
+    let oneCount = 0;
+    for (let i = 0; i < strArr.length; i++) {
+        if(strArr[i] == 0) {
+            zeroCount++
+        } else if(strArr[i] == 1) {
+            oneCount++;
+        }        
+    };
+    if(zeroCount == oneCount) {
+        return true;
+    } else {
+        return false;
+    }
+};
