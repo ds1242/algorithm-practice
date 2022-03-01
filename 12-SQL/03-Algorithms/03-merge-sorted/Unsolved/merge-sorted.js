0ab6c8fea54b5e24b101bda8eeab6880f4828986
ds1242 <david.shaw1242@gmail.com>
2022-02-28 20:54:55 -0700

    finished merge sorted algorithm

--- 12-SQL/03-Algorithms/03-merge-sorted/Unsolved/merge-sorted.js
+++ 12-SQL/03-Algorithms/03-merge-sorted/Unsolved/merge-sorted.js
@@ -1,3 +1,36 @@
 // Write code to merge two sorted arrays into a new sorted array
 
-var mergeSorted = function (arr1, arr2) {};
+var mergeSorted = function (arr1, arr2) {
+    let combinedArr = [];
+
+    let idx1 = 0;
+    let idx2 = 0;
+
+    while(idx1 < arr1.length && idx2 < arr2.length) {
+        let num1 = arr1[idx1];
+        let num2 = arr2[idx2];
+
+        if(num1 < num2) {
+            combinedArr.push(num1);
+            idx1++;
+        } else {
+            combinedArr.push(num2);
+            idx2++;
+        }
+    }
+    while (idx1 < arr1.length) {
+        var num1 = arr1[idx1];
+        combinedArr.push(num1);
+        idx1++;
+      }
+    
+      while (idx2 < arr2.length) {
+        var num2 = arr2[idx2];
+        combinedArr.push(num2);
+        idx2++;
+      }
+    
+      return combinedArr;
+
+
+};
