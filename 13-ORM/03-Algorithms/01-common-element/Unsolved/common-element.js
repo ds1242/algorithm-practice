4c78ef84abeb4dc73be168742cb2bf35e272721a
ds1242 <david.shaw1242@gmail.com>
2022-02-28 21:00:57 -0700

    solved common element search

--- 13-ORM/03-Algorithms/01-common-element/Unsolved/common-element.js
+++ 13-ORM/03-Algorithms/01-common-element/Unsolved/common-element.js
@@ -3,4 +3,18 @@
 // Return the common number
 // You may not use the `indexOf` or `includes` method
 
-var commonElement = function(arrA, arrB) {};
+var commonElement = function(arrA, arrB) {
+    let commonNum
+    for (let i = 0; i < arrA.length; i++) {
+        let currentNum1 = arrA[i];
+        for (let j = 0; j < arrB.length; j++) {
+            let currentNum2 = arrB[j];
+            if(currentNum1 === currentNum2) {
+                commonNum = currentNum1;
+            }
+        }
+
+        
+    }
+    return commonNum;
+};
