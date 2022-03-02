e62ec2e13e3548a674438dc68196c6eec16e7f8a
ds1242 <david.shaw1242@gmail.com>
2022-03-01 19:49:24 -0700

    finished largest of four alrogithm

--- freeCodeCamp/largestNumberSubArray.js
+++ freeCodeCamp/largestNumberSubArray.js
@@ -1,12 +1,15 @@
 function largestOfFour(arr) {
-    let largest = arr[0][0];
+    let largestArr = [];
     for(let i = 0; i < arr.length; i++) {
       let currentArr = arr[i];
-      for(let j = 0; j < currentArr.length; j++) {
-        if(currentArr[j] > largest) {
-          largest = currentArr[j];
+      let currentLargest = currentArr[0];
+      for(let j = 1; j < currentArr.length; j++) {
+        if(currentArr[j] > currentLargest) {
+          currentLargest = currentArr[j]
         }
       }
+          largestArr.push(currentLargest);
     }
-    return largest;
+    // console.log(largestArr)
+    return largestArr;
   }
