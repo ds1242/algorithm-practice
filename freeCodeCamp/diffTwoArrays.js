527b5c1799544fbd7a5653dc8a1d9ec7bee68084
ds1242 <david.shaw1242@gmail.com>
2022-03-08 16:30:57 -0700

    added decalartive solution to compare as well

--- freeCodeCamp/diffTwoArrays.js
+++ freeCodeCamp/diffTwoArrays.js
@@ -18,10 +18,10 @@ function diffArray(arr1, arr2) {
   
 
 //   declarative solution
-// function diffArray(arr1, arr2) {
-//     return arr1
-//       .concat(arr2)
-//       .filter(item => !arr1.includes(item) || !arr2.includes(item));
-// }
+function diffArray(arr1, arr2) {
+    return arr1
+      .concat(arr2)
+      .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
 
   diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
