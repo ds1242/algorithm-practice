b94fd3ddf6da0b163d90e06dcdbe0582222bfc8f
ds1242 <david.shaw1242@gmail.com>
2022-03-12 10:58:21 -0700

    found index of before word in string

---
+++ freeCodeCamp/searchAndReplace.js
@@ -0,0 +1,7 @@
+function myReplace(str, before, after) {
+    let strArr = str.split(' ');
+    let index = str.indexOf(before);
+    return str;
+  }
+  
+  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
