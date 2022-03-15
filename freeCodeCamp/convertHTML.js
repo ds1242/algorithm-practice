610f950f001f602eb6947d9beb61013f07ad7b62
ds1242 <david.shaw1242@gmail.com>
2022-03-14 20:24:49 -0600

    finished switch case to replace characters

--- freeCodeCamp/convertHTML.js
+++ freeCodeCamp/convertHTML.js
@@ -2,9 +2,25 @@ function convertHTML(str) {
     let strArr = str.split('');
     for(let i = 0; i < strArr.length; i++) {
       let currentChar = strArr[i];
-      
+      switch (currentChar) {
+        case "<":
+          strArr[i] = "&lt;";
+          break;
+        case "&":
+          strArr[i] = "&amp;";
+          break;
+        case ">":
+          strArr[i] = "&gt;";
+          break;
+        case '"':
+          strArr[i] = "&quot;";
+          break;
+        case "'":
+          strArr[i] = "&apos;";
+          break;
     }
     return str;
+    }
   }
   
   convertHTML("Dolce & Gabbana");
