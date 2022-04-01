b2404bd4186c696b106029f9f5d96a13bbf3da3a
ds1242 <david.shaw1242@gmail.com>
2022-03-31 20:13:48 -0600

    loop through multiples

--- freeCodeCamp/smallestCommon.js
+++ freeCodeCamp/smallestCommon.js
@@ -7,7 +7,20 @@ function smallestCommons(arr) {
     for (let i = min; i <= max; i++) {
         upperBound *= i;
     }
-    
+    // Test all multiples of 'max'
+    for (let multiple = max; multiple <= upperBound; multiple += max) {
+        // Check if every value in range divides 'multiple'
+        let divisorCount = 0;
+        for (let i = min; i <= max; i++) {
+        // Count divisors
+        if (multiple % i === 0) {
+            divisorCount += 1;
+        }
+        }
+        if (divisorCount === numberDivisors) {
+        return multiple;
+        }
+    }
   }
   
   smallestCommons([1,5]);
