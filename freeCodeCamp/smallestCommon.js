59b6e1cba2ae39d108623b3434619453e4ff5052
ds1242 <david.shaw1242@gmail.com>
2022-03-31 20:13:17 -0600

    sorted the array

--- freeCodeCamp/smallestCommon.js
+++ freeCodeCamp/smallestCommon.js
@@ -1,5 +1,13 @@
 function smallestCommons(arr) {
-    return arr;
+        // Setup
+    const [min, max] = arr.sort((a, b) => a - b);
+    const numberDivisors = max - min + 1;
+    // Largest possible value for SCM
+    let upperBound = 1;
+    for (let i = min; i <= max; i++) {
+        upperBound *= i;
+    }
+    
   }
   
   smallestCommons([1,5]);
