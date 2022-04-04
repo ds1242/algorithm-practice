a5e0aeb8d806f359c11ebbe985e2d5b74225cd6c
ds1242 <david.shaw1242@gmail.com>
2022-04-04 15:44:12 -0600

    formatting

--- freeCodeCamp/dropArr.js
+++ freeCodeCamp/dropArr.js
@@ -1,5 +1,9 @@
 function dropElements(arr, func) {
-    return arr;
+    function dropElements(arr, func) {
+        return arr;
+      }
+      
+      dropElements([1, 2, 3], function(n) {return n < 3; });
   }
   
   dropElements([1, 2, 3], function(n) {return n < 3; });
